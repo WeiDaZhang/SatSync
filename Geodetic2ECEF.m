61fa3f9c108b47f1e1db8f2461ada3efdf9c6054
weida <weida.zhang@outlook.com>
2022-04-26 00:25:54 +0000

    add input mismatch check

--- Geodetic2ECEF.m
+++ Geodetic2ECEF.m
@@ -13,6 +13,29 @@ function [x, y, z] = Geodetic2ECEF(latitude_phi, longitude_lambda, ellipsoidal_h
 % ellipsoidal_height (m), a.k.a. geodetic altitude is defined as the height
 % above the ellipsoid surface, normal to the ellipsoid.
 
+
+
+% check input length matching
+vector_of_extend = convert_length([  length(latitude_phi) ...
+                                     length(longitude_lambda) ...
+                                     length(ellipsoidal_height)]);
+if(vector_of_extend == 0)
+    disp('length mishmatch.')
+    return
+end
+
+% extend input to vector
+if(vector_of_extend(1) ~= 1)
+    latitude_phi = latitude_phi.*ones(1,vector_of_extend(1));
+end
+if(vector_of_extend(2) ~= 1)
+    longitude_lambda = longitude_lambda.*ones(1,vector_of_extend(2));
+end
+if(vector_of_extend(3) ~= 1)
+    ellipsoidal_height = ellipsoidal_height.*ones(1,vector_of_extend(3));
+end
+
+
 % earth_a (m) is the equatorial radius (semi-major axis).
 earth_a = 6378137; % meter
 
@@ -26,8 +49,8 @@ earth_b = (1 - earth_f)*earth_a;
 sqr_earth_e = (earth_a^2 - earth_b^2)/earth_a^2;
 
 % earth_N is the prime vertical radius of curvature, function of latitude_phi.
-earth_N = earth_a/sqrt(1 - sqr_earth_e*sin(latitude_phi)^2);
+earth_N = earth_a./sqrt(1 - sqr_earth_e.*sin(latitude_phi).^2);
 
-x = (earth_N + ellipsoidal_height) * cos(latitude_phi) * cos(longitude_lambda);
-y = (earth_N + ellipsoidal_height) * cos(latitude_phi) * sin(longitude_lambda);
-z = ((1 - sqr_earth_e) * earth_N + ellipsoidal_height) * sin(latitude_phi);
+x = (earth_N + ellipsoidal_height).* cos(latitude_phi) .* cos(longitude_lambda);
+y = (earth_N + ellipsoidal_height).* cos(latitude_phi) .* sin(longitude_lambda);
+z = ((1 - sqr_earth_e) .* earth_N + ellipsoidal_height) .* sin(latitude_phi);
