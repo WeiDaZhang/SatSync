a6007981b591a0bbfe8cdfdf3c0f9e111eab076c
weida <weida.zhang@outlook.com>
2022-04-26 00:20:56 +0000

    add input length match

--- ECS2ECEF.m
+++ ECS2ECEF.m
@@ -14,13 +14,36 @@ function [x, y, z] = ECS2ECEF(delta, alpha, r)
 % r (m) is the distance from the object to the centre earth
 
 r0 = 6.371e6;   % average earth radius in meter
+x = 0;
+y = 0;
+z = 0;
+
+% check input length matching
+vector_of_extend = convert_length([length(delta) length(alpha) length(r)]);
+if(vector_of_extend == 0)
+    disp('length mishmatch.')
+    return
+end
+
+% check distance input
 if(r < r0)
     r = r + r0;
     disp('Input distance smaller than average earth radius.');
     disp('Input distance considered as max orbital height.');
 end
 
-x = cos(delta)*cos(alpha)*r;
-y = cos(delta)*sin(alpha)*r;
-z = sin(delta)*r;
+% extend input to vector
+if(vector_of_extend(1) ~= 1)
+    delta = delta.*ones(1,vector_of_extend(1));
+end
+if(vector_of_extend(2) ~= 1)
+    alpha = alpha.*ones(1,vector_of_extend(2));
+end
+if(vector_of_extend(3) ~= 1)
+    r = r.*ones(1,vector_of_extend(3));
+end
+
+x = cos(delta).*cos(alpha).*r;
+y = cos(delta).*sin(alpha).*r;
+z = sin(delta).*r;
 return
