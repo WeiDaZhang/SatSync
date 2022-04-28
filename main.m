484457168a6075695712bbfffd565462ae8c0da2
weidazhang <weidazhang@CEGWT8.physics.ox.ac.uk>
2022-04-28 19:20:32 +0100

    fix f0 changes with nOverSample

--- main.m
+++ main.m
@@ -23,20 +23,35 @@ plot3(0,0,0,'+')
 
 satellite_omega = SatelliteAngularVelocity(satellite_altitude + average_earth_radius);
 
+% Number of Point
+N = 2^16;
+
+% Symbol Rate (Repetition Rate of the whole Spectrum Spread Code Sequence)
+SymbolRate = 1e3;
+
+% 1023 LFSR Polinomial x^10 + x^7 + 1
+% 4095 LFSR Polinomial x^12 + x^11 + x^10 + x^4 + 1
+ChipLength = 1023;
+LFSR_POL = [10 7];
+
+% Chip Rate (Rate of Spectrum Spread Code)
+% For simplicity, ChipRate should be integer multiple of LFSR length
+ChipRate = ChipLength * SymbolRate;
+
+% Oversampling Rate (How many samples each chip duration)
+nOverSample = 4;
+
 % Signal Carrier frequency
-f0 = 1.57542e9; %(Hz)
-f0 = 1.584765e9; %(Hz)
+% For simplicity f0 should be (integer + 1/4) multiple of (Chip Rate) * (Over Sample)
+% f0 = 1.57542e9; %(Hz)
+f0 = ChipRate*nOverSample*(1536/nOverSample + 1/4); %(Hz)
+% f0 = ChipRate*nOverSample*(1536/nOverSample + 1/4); %(Hz)
+fprintf('Carrier Frequency: %f GHz\n', f0/1e9);
 lambda0 = c/f0;
 w0 = 2*pi*f0;
 k0 = w0/c;
 p0dBm = 40; % (dBm)
 
-% Oversampling Rate (How many samples each chip duration)
-nOverSample = 12;
-% Chip Rate (Rate of Spectrum Spread Code)
-ChipRate = 4.095e6;
-% Number of Point
-N = 2^25;
 % Local Time
 t = 0 : 1/ChipRate/nOverSample : (N-1)/ChipRate/nOverSample;
 
@@ -63,10 +78,16 @@ link_loss = 20*log10(4*pi*range_x/lambda0);
 received_power = p0dBm - link_loss;
 received_amp = sqrt(10.^(received_power/10)*50);
 
-% 1023 LFSR Polinomial x^10 + x^7 + 1
-LFSR_POL = [12 11 10 4];
 LFSR_INIT = [0 0 0 0 0 1];
 ChipCode_b = LFSR_t(t_prime - range_x/c, ChipRate, LFSR_INIT, LFSR_POL);
 
 received_r = received_amp.*sin(w0*t_prime - k0*range_x + ChipCode_b*pi);
-figure;plot(abs(fft(received_r)))
+% figure;plot(abs(fft(received_r)))
+
+IQBB = RF_FrontEnd(received_r, ChipRate, nOverSample);
+
+figure;plot(abs(fft(IQBB)));
+figure;plot(real(IQBB));
+figure;plot(imag(IQBB));
+
+BasebandProcessing(IQBB, ChipLength, ChipRate, nOverSample, LFSR_POL)
