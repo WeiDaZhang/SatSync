22494985227bd3403fb3fc225a7ae4d4349ec117
weidazhang <weidazhang@CEGWT8.physics.ox.ac.uk>
2022-05-03 17:59:40 +0100

    Change LFSR to single chip length generation

--- LFSR_t.m
+++ LFSR_t.m
@@ -1,9 +1,10 @@
 % Linear Feedback Shift Register by Time
-function seq = LFSR_t(t, rate, state, tap)
+function seq = LFSR_t(t, rate, state, tap, chip_length)
 % t (s) is array of time in seconds
 % rate (Hz) is the symbol rate of LFSR
 % state is the vector of initial state
 % tap is the feedback polinomial, i.e. x^5 +x^2 + 1 == [5 2]
+% chip_length is the length of the chip
 
 if(max(tap) > length(state))
     MSB = max(tap);
@@ -27,20 +28,19 @@ end
 N = length(t);
 long_tap = length(tap) > 2;     % extra xor needed if tap number > 2
 b = false;
-seq = zeros(1,N);
-rounds = 0;
-for idx = 1:N
-    last_round = rounds;
-    rounds = floor(t(idx)*rate);
-    for idx_round = 1:(rounds - last_round)
-        b = xor(state(tap(1)),state(tap(2)));
-        if(long_tap)
-            for idx_tap = 3:length(tap)
-                b = xor(b, state(tap(idx_tap)));
-            end
+chip = zeros(1, chip_length);
+seq = zeros(1, N);
+for idx_chip = 1:chip_length
+    b = xor(state(tap(1)),state(tap(2)));
+    if(long_tap)
+        for idx_tap = 3:length(tap)
+            b = xor(b, state(tap(idx_tap)));
         end
-        state = [b state(1:end - 1)];
     end
-    seq(idx) = state(end);
+    state = [b state(1:end - 1)];
+    chip(idx_chip) = state(end);
 end
-    
+
+for idx = 1:N
+    seq(idx) = chip(mod(floor(t(idx)*rate), chip_length) + 1)*2-1;
+end
