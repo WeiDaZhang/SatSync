47eb73629ee42cffef337139f96ca50472dc911b
weidazhang <weidazhang@CEGWT8.physics.ox.ac.uk>
2022-05-04 19:13:41 +0100

    Compose half band filter in baseband processing module, fft based filter is disabled

--- RF_FrontEnd.m
+++ RF_FrontEnd.m
@@ -10,5 +10,5 @@ if nargin > 3
     f_IF = f_IF_in;
 end
 IQBB = s_RF .* exp(1j*2*pi*f_IF*t);
-f_IQBB = fft(IQBB);
-IQBB = ifft([f_IQBB(1:floor(N/4)) zeros(1, N-2*floor(N/4)) f_IQBB(N-floor(N/4)+1:N)]);
+% f_IQBB = fft(IQBB);
+% IQBB = ifft([f_IQBB(1:floor(N/4)) zeros(1, N-2*floor(N/4)) f_IQBB(N-floor(N/4)+1:N)]);
